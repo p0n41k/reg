3763691e3ad9669eefa9334982e7edb551c531d3
ynurmakh <nur_erbol_2002@mail.ru>
2023-10-24 12:33:44 +0600

    m

---
+++ block.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+# Set pass  "-+-+" after start script
+
+xset s 60
+xrandr --output  DP-1 --brightness 0
+xdotool mousemove 50 550 click 1
+sleep 1
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key Return
+sleep 2
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key Return
+xdotool mousemove 50 550 click 1
+sleep 1
+xdotool key Return
+sleep 70
+xrandr --output  DP-1 --brightness 1
+
+sleep 1500
+xrandr --output  DP-1 --brightness 0
+sleep 2
+xdotool key KP_Subtract
+sleep 0.1
+xdotool key KP_Add
+sleep 0.1
+xdotool key KP_Subtract
+sleep 0.1
+xdotool key KP_Add
+xdotool key Return
+sleep 0.5
+xdotool mousemove 50 50 click 1chmod 
+sleep 2.5
+xdotool key ctrl+t
+sleep 0.1
+xdotool key 0
+xdotool key 1
+xdotool key period
+xdotool key a
+xdotool key l
+xdotool key e
+xdotool key m
+xdotool key period
+xdotool key s
+xdotool key c
+xdotool key h
+xdotool key o
+xdotool key o
+xdotool key l
+xdotool key Return
+sleep 1.5
+sleep 1.5
+
+xdotool key ctrl+t
+xdotool key d
+xdotool key a
+xdotool key s
+xdotool key h
+xdotool key b
+xdotool key o
+xdotool key a
+xdotool key r
+xdotool key d
+xdotool key period
+xdotool key a
+xdotool key l
+xdotool key e
+xdotool key m
+xdotool key period
+xdotool key s
+xdotool key c
+xdotool key h
+xdotool key o
+xdotool key o
+xdotool key l
+xdotool key Return
+sleep 1.5
+sleep 1.5
+
+xdotool mousemove 50 550 click 1chmod 
+sleep 2
+
+xdotool key Return
+sleep 2
+xrandr --output  DP-1 --brightness 1
+
+sleep 1500 # sleep time
+# xrandr --output  DP-1 --brightness 0
+sleep 2
+xdotool key KP_Subtract
+sleep 0.1
+xdotool key KP_Add
+sleep 0.1
+xdotool key KP_Subtract
+sleep 0.1
+xdotool key KP_Add
+xdotool key Return
+sleep 0.5
+xdotool mousemove 50 50 click 1chmod 
+
+sleep 2.5
+xdotool key ctrl+t
+sleep 0.1
+xdotool key 0
+xdotool key 1
+xdotool key period
+xdotool key a
+xdotool key l
+xdotool key e
+xdotool key m
+xdotool key period
+xdotool key s
+xdotool key c
+xdotool key h
+xdotool key o
+xdotool key o
+xdotool key l
+xdotool key Return
+sleep 1.5
+sleep 1.5
+sleep 1.5
+
+xdotool key ctrl+t
+
+xdotool key d
+xdotool key a
+xdotool key s
+xdotool key h
+xdotool key b
+xdotool key o
+xdotool key a
+xdotool key r
+xdotool key d
+xdotool key period
+xdotool key a
+xdotool key l
+xdotool key e
+xdotool key m
+xdotool key period
+xdotool key s
+xdotool key c
+xdotool key h
+xdotool key o
+xdotool key o
+xdotool key l
+xdotool key Return
+sleep 1.5
+sleep 1.5
+
+
+xdotool mousemove 50 550 click 1chmod 
+sleep 2
+
+xdotool key Return
+sleep 2
+xrandr --output  DP-1 --brightness 1
