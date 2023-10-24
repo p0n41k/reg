935ba78fc352f4cf4df055cba577c34361b54e7b
ynurmakh <nur_erbol_2002@mail.ru>
2023-10-24 12:59:08 +0600

    l

--- block.sh
+++ block.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-xset s 60
+xset s 5
 xrandr --output  DP-1 --brightness 0
 xdotool mousemove 50 550 click 1
 sleep 1
@@ -19,9 +19,77 @@ sleep 2
 xdotool mousemove 50 550 click 1
 sleep 1
 xdotool key Return
-sleep 70
+sleep 10
 xrandr --output  DP-1 --brightness 1
+xset s 300
 
+
+# ===================================================
+sleep 1500
+xrandr --output  DP-1 --brightness 0 
+sleep 1
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key Return
+sleep 0.5
+xdotool mousemove 50 50 click 1 
+sleep 2.5
+xdotool key ctrl+t
+sleep 0.1
+xdotool key 0
+xdotool key 1
+xdotool key period
+xdotool key a
+xdotool key l
+xdotool key e
+xdotool key m
+xdotool key period
+xdotool key s
+xdotool key c
+xdotool key h
+xdotool key o
+xdotool key o
+xdotool key l
+sleep 0.1
+xdotool key Return
+sleep 0.1
+xdotool key ctrl+t
+xdotool key d
+xdotool key a
+xdotool key s
+xdotool key h
+xdotool key b
+xdotool key o
+xdotool key a
+xdotool key r
+xdotool key d
+xdotool key period
+xdotool key a
+xdotool key l
+xdotool key e
+xdotool key m
+xdotool key period
+xdotool key s
+xdotool key c
+xdotool key h
+xdotool key o
+xdotool key o
+xdotool key l
+xdotool key Return
+sleep 3
+xdotool mousemove 1700 850 click 1
+sleep 1
+
+xdotool mousemove 50 550 click 1
+sleep 2
+xdotool key Return
+xset s 5
+sleep 10
+xrandr --output  DP-1 --brightness 1
+xset s 300
+# ===================================================
 # ===================================================
 sleep 1500
 xrandr --output  DP-1 --brightness 0
@@ -83,10 +151,11 @@ sleep 1
 xdotool mousemove 50 550 click 1
 sleep 2
 xdotool key Return
-sleep 70
+xset s 5
+sleep 10
 xrandr --output  DP-1 --brightness 1
-# =============================================
-
+xset s 300
+# ===================================================
 # ===================================================
 sleep 1500
 xrandr --output  DP-1 --brightness 0
@@ -148,10 +217,11 @@ sleep 1
 xdotool mousemove 50 550 click 1
 sleep 2
 xdotool key Return
-sleep 70
+xset s 5
+sleep 10
 xrandr --output  DP-1 --brightness 1
-# =============================================
-
+xset s 300
+# ===================================================
 # ===================================================
 sleep 1500
 xrandr --output  DP-1 --brightness 0
@@ -213,71 +283,8 @@ sleep 1
 xdotool mousemove 50 550 click 1
 sleep 2
 xdotool key Return
-sleep 70
+xset s 5
+sleep 10
 xrandr --output  DP-1 --brightness 1
-# =============================================
-
+xset s 300
 # ===================================================
-sleep 1500
-xrandr --output  DP-1 --brightness 0
-sleep 1
-xdotool key KP_Subtract
-xdotool key KP_Add
-xdotool key KP_Subtract
-xdotool key KP_Add
-xdotool key Return
-sleep 0.5
-xdotool mousemove 50 50 click 1 
-sleep 2.5
-xdotool key ctrl+t
-sleep 0.1
-xdotool key 0
-xdotool key 1
-xdotool key period
-xdotool key a
-xdotool key l
-xdotool key e
-xdotool key m
-xdotool key period
-xdotool key s
-xdotool key c
-xdotool key h
-xdotool key o
-xdotool key o
-xdotool key l
-sleep 0.1
-xdotool key Return
-sleep 0.1
-xdotool key ctrl+t
-xdotool key d
-xdotool key a
-xdotool key s
-xdotool key h
-xdotool key b
-xdotool key o
-xdotool key a
-xdotool key r
-xdotool key d
-xdotool key period
-xdotool key a
-xdotool key l
-xdotool key e
-xdotool key m
-xdotool key period
-xdotool key s
-xdotool key c
-xdotool key h
-xdotool key o
-xdotool key o
-xdotool key l
-xdotool key Return
-sleep 3
-xdotool mousemove 1700 850 click 1
-sleep 1
-
-xdotool mousemove 50 550 click 1
-sleep 2
-xdotool key Return
-sleep 70
-xrandr --output  DP-1 --brightness 1
-# =============================================
