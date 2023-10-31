94fbf895598e61ef9c849ad932454b4efd2792bb
ynurmakh <nur_erbol_2002@mail.ru>
2023-10-31 18:00:27 +0600

    123

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+xdotool mousemove 50 550 click 1
+sleep 1
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key Return
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key KP_Subtract
+xdotool key KP_Add
+xdotool key Return
