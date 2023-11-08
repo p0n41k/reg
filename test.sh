c5bc7b165a91a2e42d8cee82f3b7778f065a69d3
ynurmakh <nur_erbol_2002@mail.ru>
2023-11-08 17:38:58 +0600

    123

--- test.sh
+++ test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-xdotool mousemove 1600 630 click 1
+xdotool mousemove 1800 800
 # sleep 1
 
 
 # xdotool mousemove 1600 630
 
-while true; do
-  color=$(import -window root -crop 1x1+1600+630 -depth 8 txt:- | awk 'NR==2 {print $3}')
-  if [ "$color" == "#2C2C2C" ]; then
-  echo nashel $color
-  sleep 0.5
-  break
-  else
-  echo ne tot $color
-  sleep 0.5
-  fi
-done
+# while true; do
+#   color=$(import -window root -crop 1x1+1600+630 -depth 8 txt:- | awk 'NR==2 {print $3}')
+#   if [ "$color" == "#2C2C2C" ]; then
+#   echo nashel $color
+#   sleep 0.5
+#   break
+#   else
+#   echo ne tot $color
+#   sleep 0.5
+#   fi
+# done
 
-echo "tut"
+# echo "tut"
