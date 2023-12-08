78640eff886dc60a32e03c9b1ee2add9d7b107a1
ynurmakh <nur_erbol_2002@mail.ru>
2023-12-08 15:52:56 +0600

    123

--- lock.sh
+++ lock.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cd /home/student/Pictures
-rm *
+# cd /home/student/Pictures
+# rm *
 
-xdotool key Print
-sleep 1
+# xdotool key Print
+# sleep 1
 
-img=$(ls "/home/student/Pictures")
-echo "$img"
-i3lock -i "$img"
+# img=$(ls "/home/student/Pictures")
+# echo "$img"
+i3lock -i /usr/share/backgrounds/alem-for-convert.png
