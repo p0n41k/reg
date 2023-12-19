8fb44d4c31dcb5d88a5cdd1cc1cda1acac3b3f48
ynurmakh <nur_erbol_2002@mail.ru>
2023-12-19 18:29:55 +0600

    123

--- Fast_Reg_By_p0n41k/test.sh
+++ Fast_Reg_By_p0n41k/test.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
 
+tim=10
+while tim >0;do
+    sleep 1
+    ((tim--))
+    echo $tim
+done
+
+exit 1
+
 xdotool mousemove 50 1400 click 1
 xdotool type "Chrome"
 xdotool key Return
 
-while true; do
-  color=$(import -window root -crop 1x1+1600+700 -depth 8 txt:- | awk 'NR==2 {print $3}')
-  if [ "$color" == "#FAFAFA" ]; then
-  sleep 0.2
-  break
-  else
-  sleep 0.2
-  fi
-done
-xdotool key Escape
+
+
+
+
+
 
 while true; do
-  color=$(import -window root -crop 1x1+1600+700 -depth 8 txt:- | awk 'NR==2 {print $3}')
-  if [ "$color" == "#FAFAFA" ]; then
-  sleep 0.2
-  break
-  else
-  sleep 0.2
-  fi
+    color=$(import -window root -crop 1x1+1600+700 -depth 8 txt:- | awk 'NR==2 {print $3}')
+    if [ "$color" == "#FAFAFA" ]; then
+        sleep 0.2
+        xdotool key Escape
+        break
+    else
+        sleep 0.2
+    fi
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while true; do
+    color=$(import -window root -crop 1x1+1600+700 -depth 8 txt:- | awk 'NR==2 {print $3}')
+    if [ "$color" == "#FAFAFA" ]; then
+        sleep 0.2
+        break
+    else
+        sleep 0.2
+    fi
 done
 
 xdotool key Escape
