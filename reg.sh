88373932c7213bba2d114d11c4f10f6f1b8a823b
ynurmakh <nur_erbol_2002@mail.ru>
2023-12-20 18:13:57 +0600

    123

--- reg.sh
+++ reg.sh
@@ -4,10 +4,27 @@
 
 # Открываем браузер и производим регистрацию
 xdotool mousemove 50 50 click 1
-sleep 3
-xdotool mousemove 3420 100 click 1
-sleep 0.2
-xdotool mousemove 3400 130 click 1 
+# sleep 3
+# xdotool mousemove 3420 100 click 1
+# sleep 0.2
+# xdotool mousemove 3400 130 click 1 
+
+
+while true; do
+    color=$(import -window root -crop 1x1+200+300 -depth 8 txt:- | awk 'NR==2 {print $3}')
+    if [ "$color" == "#2B2A33" ]; then
+        sleep 0.2
+        break
+    else
+        sleep 0.2
+    fi
+done
+
+
+xdotool type "https://accounts.firefox.com/?context=fx_desktop_v3&entrypoint=fxa_app_menu&action=email&service=sync"
+xdotool key Return
+
+
 
 # Ждем загрузки страницы
 while true; do
